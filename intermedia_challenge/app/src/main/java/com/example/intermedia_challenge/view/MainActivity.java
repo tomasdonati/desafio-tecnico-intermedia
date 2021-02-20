fb79a388a675827458de1e9117519ceff41f79ac
tomasdonati <tomasdonati16@gmail.com>
2021-02-20 00:17:47 -0300

    primer commit

---
+++ intermedia_challenge/app/src/main/java/com/example/intermedia_challenge/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.intermedia_challenge.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.intermedia_challenge.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
