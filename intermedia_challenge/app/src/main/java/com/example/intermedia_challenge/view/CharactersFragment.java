42f172922b2e74f2fc04a70c747b9d4c3392662d
tomasdonati <tomasdonati16@gmail.com>
2021-02-21 17:04:11 -0300

    Added BottomNav, no funcionality yet.
    Simply added bottomNavBar, changes between fragments but no further funcionality in each one.

---
+++ intermedia_challenge/app/src/main/java/com/example/intermedia_challenge/view/CharactersFragment.java
@@ -0,0 +1,26 @@
+package com.example.intermedia_challenge.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.intermedia_challenge.R;
+
+public class CharactersFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_characters, container, false);
+
+
+
+
+        return view;
+    }
+}
--- intermedia_challenge/app/src/main/java/com/example/intermedia_challenge/view/MainActivity.java
+++ intermedia_challenge/app/src/main/java/com/example/intermedia_challenge/view/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.intermedia_challenge.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.intermedia_challenge.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.mainactivity_bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainactivity_fragment_container,
+                new CharactersFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.bottomNavBtnCharacters:
+                            selectedFragment = new CharactersFragment();
+                            break;
+
+                        case R.id.bottomNavBtnEvents:
+                            selectedFragment = new EventsFragment();
+                            break;
+
+                        case R.id.bottomNavBtnInfo:
+                            selectedFragment = new InformationFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainactivity_fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
